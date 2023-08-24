06d849b3b83e4f23d9a74e4d2683318d1f57b3c9
guerric sant <guerric.sant@gmail.com>
2023-08-24 10:41:37 +0200

    models, validations, associations done

---
+++ app/models/bookmark.rb
@@ -0,0 +1,9 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, length: { minimum: 6 }
+  validates_associated :movie
+  validates_associated :list
+  validates :movie, uniqueness: { scope: :list }
+end
